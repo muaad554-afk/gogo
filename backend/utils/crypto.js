13bfee46672a96652e7ca1d1bb3f2d1957ff4a91
muaad554-afk <muaad554@gmail.com>
2025-08-06 21:49:31 +0300

    Create crypto.js

---
+++ backend/utils/crypto.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto");
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 32 bytes
+const IV_LENGTH = 16; // For AES, this is always 16
+
+if (!ENCRYPTION_KEY || ENCRYPTION_KEY.length !== 32) {
+  throw new Error("ENCRYPTION_KEY must be set in .env and be 32 characters long");
+}
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted;
+}
+
+function decrypt(encrypted) {
+  const [ivHex, encryptedText] = encrypted.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt };
