59d380e11d5b45db10fa6da51ee0382f2b3aee4d
muaad554-afk <muaad554@gmail.com>
2025-08-07 22:30:42 +0300

    Create settings.jsx

---
+++ my-frontend/pages/settings.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import Settings from "../components/Settings";
+
+export default function SettingsPage() {
+  return (
+    <Layout>
+      <Settings />
+    </Layout>
+  );
+}
