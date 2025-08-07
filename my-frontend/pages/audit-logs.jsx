b1ebd44421420571ebeac6643e0192f9c5d24ebb
muaad554-afk <muaad554@gmail.com>
2025-08-07 22:31:29 +0300

    Create audit-logs.jsx

---
+++ my-frontend/pages/audit-logs.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import AuditLogs from "../components/AuditLogs";
+
+export default function AuditLogsPage() {
+  return (
+    <Layout>
+      <AuditLogs />
+    </Layout>
+  );
+}
