30e60f8d04108e34bc8f7a353e1f290f928d1014
muaad554-afk <muaad554@gmail.com>
2025-08-07 14:18:12 +0300

    Update Credentials.js

--- backend/models/Credentials.js
+++ backend/models/Credentials.js
@@ -1,22 +1,36 @@
-const { db } = require('../config/db');
+const db = require("../config/db");
+const { encrypt, decrypt } = require("../utils/crypto");
 
-function saveCredentials(clientId, encryptedKeys, callback) {
-  db.run(
-    `INSERT OR REPLACE INTO credentials (client_id, stripe_key, paypal_key, slack_url, openai_key)
-     VALUES (?, ?, ?, ?, ?)`,
-    [
-      clientId,
-      encryptedKeys.stripe_key,
-      encryptedKeys.paypal_key,
-      encryptedKeys.slack_url,
-      encryptedKeys.openai_key
-    ],
-    callback
-  );
+class Credentials {
+  static async save(client_id, { stripe_key, paypal_key, slack_url, openai_key }) {
+    const encStripe = stripe_key ? encrypt(stripe_key) : null;
+    const encPaypal = paypal_key ? encrypt(paypal_key) : null;
+    const encSlack = slack_url ? encrypt(slack_url) : null;
+    const encOpenAI = openai_key ? encrypt(openai_key) : null;
+
+    const sql = `
+      INSERT INTO credentials (client_id, stripe_key, paypal_key, slack_url, openai_key)
+      VALUES (?, ?, ?, ?, ?)
+      ON CONFLICT(client_id) DO UPDATE SET
+        stripe_key=excluded.stripe_key,
+        paypal_key=excluded.paypal_key,
+        slack_url=excluded.slack_url,
+        openai_key=excluded.openai_key
+    `;
+    await db.run(sql, [client_id, encStripe, encPaypal, encSlack, encOpenAI]);
+  }
+
+  static async getByClientId(client_id) {
+    const row = await db.get("SELECT * FROM credentials WHERE client_id = ?", [client_id]);
+    if (!row) return null;
+
+    return {
+      stripeKey: row.stripe_key ? decrypt(row.stripe_key) : null,
+      paypalKey: row.paypal_key ? decrypt(row.paypal_key) : null,
+      slackUrl: row.slack_url ? decrypt(row.slack_url) : null,
+      openAiKey: row.openai_key ? decrypt(row.openai_key) : null,
+    };
+  }
 }
 
-function getCredentialsByClientId(clientId, callback) {
-  db.get(`SELECT * FROM credentials WHERE client_id = ?`, [clientId], callback);
-}
-
-module.exports = { saveCredentials, getCredentialsByClientId };
+module.exports = Credentials;
