898ef91870ccbef3fa05eb656e89aaafe5a217a8
muaad554-afk <muaad554@gmail.com>
2025-08-07 22:30:21 +0300

    Create refunds.jsx

---
+++ my-frontend/pages/refunds.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import RefundList from "../components/RefundList";
+
+export default function Refunds() {
+  return (
+    <Layout>
+      <RefundList />
+    </Layout>
+  );
+}
