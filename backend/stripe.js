adb8a60cf804db18488e849c1f99ef3b81fa4f67
muaad554-afk <muaad554@gmail.com>
2025-08-07 07:12:04 +0300

    Update stripe.js

--- backend/stripe.js
+++ backend/stripe.js
@@ -1,1 +1,13 @@
+const Stripe = require("stripe");
+
+async function refundStripePayment(apiKey, paymentIntentId, amount) {
+  const stripe = new Stripe(apiKey);
+  return await stripe.refunds.create({
+    payment_intent: paymentIntentId,
+    amount: Math.round(amount * 100),
+    reason: "requested_by_customer"
+  });
+}
+
+module.exports = { refundStripePayment };
 
